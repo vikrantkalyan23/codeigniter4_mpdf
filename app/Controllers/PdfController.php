1ca44a9c2b30da493c439ce666c57e210a380c95
vikrantkalyan23 <vikrant.kalyan23@gmail.com>
2025-02-01 14:43:58 +0530

    #update

--- app/Controllers/PdfController.php
+++ app/Controllers/PdfController.php
@@ -2,14 +2,14 @@
 
 namespace App\Controllers;
 
-use App\Libraries\MpdfGenerator;
+use App\Libraries\MpdfLibrary;
 use CodeIgniter\Controller;
 
 class PdfController extends Controller
 {
     public function generatePDF()
     {
-        $pdf = new MpdfGenerator();
+        $pdf = new MpdfLibrary();
         $html = '<h1>Hello, CodeIgniter 4 with mPDF!</h1>
                  <p>This is a dynamically generated PDF document.</p>';
         $this->response->setHeader('Content-Type', 'application/pdf');
